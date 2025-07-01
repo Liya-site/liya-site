878e66b97bbeb25475b05647f41395fa33f49690
daim43012 <vgatsko.21@gmail.com>
2025-07-01 13:55:41 +0300

    add heared and footer as a components

---
+++ src/components/parts/Footer.svelte
@@ -0,0 +1,110 @@
+<!--  Footer  -->
+<footer id="footer" class="footer-section">
+    <div class="container">
+      <div class="footer-content pb-5">
+        <div class="row">
+          <div class="col-xl-4 col-lg-4 mb-50">
+            <div class="footer-widget">
+              <div class="footer-logo">
+                <a href="#" class="logo d-flex align-items-center">
+                  <img src="assets/images/log.png" alt="logo" />
+                </a>
+              </div>
+              <div class="footer-text">
+                <p>
+                  Full-Service Business Development and digital growth — From
+                  Product Creation to Social Media.
+                </p>
+              </div>
+              <div class="footer-social-icon">
+                <span>Follow us</span>
+                <a href="https://www.linkedin.com/in/liya-khatkova-062320364"
+                  ><i class="bi bi-linkedin"></i></a
+                >
+                <a
+                  href="https://www.instagram.com/keyscalelab?igsh=MTNqa2dtMWd5eG5rbA=="
+                  ><i class="bi bi-instagram"></i></a
+                >
+              </div>
+            </div>
+          </div>
+  
+          <div class="col-lg-2 col-md-6 col-sm-12 footer-column">
+            <div class="service-widget footer-widget">
+              <div class="footer-widget-heading">
+                <h3>Services</h3>
+              </div>
+              <ul class="list">
+                <!--<li><a href="single-service.html">Web Design</a></li>
+                  <li><a href="single-service.html">App Developemnt</a></li>
+                  <li><a href="single-service.html">Cloud Services</a></li>
+                  <li><a href="single-service.html">Domain And Hosting</a></li>
+                  <li><a href="single-service.html">Seo Optimization</a></li>
+                  <li><a href="single-service.html">Social Media</a></li>
+                  <li><a href="single-service.html">Data Secure</a></li>
+                  <li><a href="single-service.html">Web Hosting</a></li> -->
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-6 col-sm-12 footer-column">
+            <div class="service-widget footer-widget">
+              <div class="footer-widget-heading">
+                <h3>Information</h3>
+              </div>
+              <ul class="list">
+                <!--<li><a href="about.html">About</a></li>
+                  <li><a href="pricing.html">Pricing</a></li>
+                  <li><a href="faqs.html">FAQs</a></li>
+                  <li><a href="team.html">Team</a></li>
+                  <li><a href="blogs.html">Blogs</a></li>
+                  <li><a href="single-blog.html">Single Blog</a></li>
+                  <li><a href="page.html">Terms &amp; Conditions</a></li>
+                  <li><a href="page.html">Sample Page</a></li>-->
+              </ul>
+            </div>
+          </div>
+          <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+            <div class="contact-widget footer-widget mb-25">
+              <div class="footer-widget-heading">
+                <h3>Contacts</h3>
+              </div>
+              <div class="footer-text">
+                <p>
+                  <i class="bi bi-geo-alt-fill mr-15"></i> 32 Kyriakou Matsi, Nicosia,
+                  Cyprus
+                </p>
+                <p>
+                  <i class="bi bi-telephone-inbound-fill mr-15"></i> +375-94-48-37-18
+                </p>
+                <p>
+                  <i class="bi bi-envelope-fill mr-15"></i> lillu.ocean@gmail.com
+                </p>
+              </div>
+            </div>
+            <div class="footer-widget">
+              <div class="footer-widget-heading">
+                <h3>Newsletter</h3>
+              </div>
+              <div class="footer-text mb-25">
+                <p>Subscribe to our new feeds, kindly fill the form below.</p>
+              </div>
+              <div class="subscribe-form">
+                <form action="#">
+                  <input type="text" placeholder="Email Address" />
+                  <button>Subscribe<span></span></button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xl-12 col-lg-12 text-center">
+            <div class="copyright-text">
+              <p>KeyScaleLab<span>.</span> © 2025</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
---
+++ src/components/parts/Header.svelte
@@ -0,0 +1,50 @@
+<!-- Start Header -->
+<header
+  id="header"
+  class="header d-flex align-items-center sticked stikcy-menu"
+>
+  <div
+    class="container-fluid container-xl d-flex align-items-center justify-content-between"
+  >
+    <a href="index.html" class="logo d-flex align-items-center">
+      <img src="assets/images/log.png" alt="logo" />
+    </a>
+    <nav id="navbar" class="navbar">
+      <ul>
+          <li><a href="/" class="">Home</a></li>
+          <li><a href="/about" class="">About Us</a></li>
+          <!-- <li class="dropdown"><a href="services.html"><span>Services</span> <i
+                class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="services.html">Services</a></li>
+              <li><a href="single-service.html">Single Service</a></li>
+              <li><a href="pricing.html">Pricing</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="portfolio.html"><span>Portfolio</span> <i
+                class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="portfolio.html">Projects</a></li>
+              <li><a href="single-project.html">Single Project</a></li>
+            </ul>
+          </li> -->
+          <li><a href="/team" class="">Team</a></li>
+          <!-- <li class="dropdown"><a href="blogs.html"><span>Blogs</span> <i
+                class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="blogs.html">Blogs Grid</a></li>
+              <li><a href="single-blog.html">Blog Details</a></li>
+            </ul>
+          </li> -->
+        </ul>
+    </nav>
+    <!-- .navbar -->
+    <!--<a href="" class="d-none d-md-flex default-theme-btn-one">Contact Us <span></span></a>-->
+    <a class="default-theme-btn-one" href="mailto:lillu.ocean@gmail.com"
+      >Contact Us <span></span></a
+    >
+    <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+    <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+  </div>
+</header>
+<!-- End Header -->
